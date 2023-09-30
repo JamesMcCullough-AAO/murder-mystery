4cc95d59f86932c1f456081d8fc3e8fb2d243679
James McCullough <james.mccullough@aaoglobal.com>
2023-09-30 22:42:52 +0100

    parsing response

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import ChatInterface from "./utils/chatInterface";
+import ChatInterface from "./components/chatInterface";
 
 function App() {
   return <ChatInterface />;
---
+++ src/utils/parseObjectResponse.ts
@@ -0,0 +1,25 @@
+// A function to parse a response from the AI. Responses should be an object, starting with an object name and a colon. They should end with a close }
+// Remove any text before the given object name, and remove the object name and colon from the start of the response.
+// Remove any text after the close }.
+
+export const isValidObjectResponse = (response: string, objectName: string) => {
+  const start = response.indexOf(objectName);
+  const end = response.indexOf("}", start);
+  if (start === -1 || end === -1) {
+    return false;
+  }
+  // If the object can be parsed, it's valid.
+  try {
+    JSON.parse(response.substring(start, end + 1));
+  } catch (error) {
+    return false;
+  }
+  return true;
+};
+
+export const parseObjectResponse = (response: string, objectName: string) => {
+  const start = response.indexOf(objectName);
+  const end = response.indexOf("}", start);
+  const parsedResponse = response.substring(start, end + 1);
+  return JSON.parse(parsedResponse);
+};
